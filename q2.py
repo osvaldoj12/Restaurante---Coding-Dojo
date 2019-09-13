5701b2dfff02d8fc75b39a44043bfe099f864890
Osvaldo <osvaldojuniorlima@hotmail.com>
2019-09-13 14:18:56 -0300

    Trerceira e ultima parte, finalizando com import e instancia

---
+++ q2.py
@@ -0,0 +1,9 @@
+from Restaurante_pt2 import Restaurante
+
+r1 = Restaurante("Gordelicia","Engorde Aqui")
+r2 = Restaurante("Macarrao Rei","...")
+r3 = Restaurante("Esta com Fome?", "Nao venha aqui")
+
+r1.describe_restaurant()
+r2.describe_restaurant()
+r3.describe_restaurant()
