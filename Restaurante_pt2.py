718c9c98fde32ff953b9a0975326276be8875326
Osvaldo <osvaldojuniorlima@hotmail.com>
2019-09-13 14:18:05 -0300

    Segunda parte do Restaurante, utilizando import

---
+++ Restaurante_pt2.py
@@ -0,0 +1,9 @@
+from Restaurante import Restaurante
+
+restaurant = Restaurante("Delicia", "Indiana")
+
+print(restaurant.restaurant_name)
+print(restaurant.cuisine_type)
+
+restaurant.describe_restaurant()
+restaurant.open_restaurant()
